c53aa1ac48c56d13e2f0704f9abf2cae0d395feb
kankankankankankan <vip029@me.com>
2023-02-21 17:58:09 +0800

    Update navBarItem.js

--- src/layouts/components/header/navBarItem.js
+++ src/layouts/components/header/navBarItem.js
@@ -1,17 +1,3 @@
 export const navBarItem = [
-  {
-    title: '首页',
-    link: '/',
-    target: '',
-  },
-  {
-    title: '短链接',
-    link: process.env.VUE_APP_SHORT_URL,
-    target: '_blank',
-  },
-  {
-    title: 'GitHub',
-    link: 'https://github.com/stilleshan/subweb',
-    target: '_blank',
-  },
+
 ];
